3ca71b05fa70e4c948df2a8eeaee7bbf6b368e61
GbeTech <gbetech@gmail.com>
2018-09-11 02:59:19 +0300

    all but def and class pass

--- src/operators/print_operator.py
+++ src/operators/print_operator.py
@@ -6,41 +6,52 @@ from src.utils import xnor
 class PrintOperator(Operator, cls_keywords=('print',)):
 
     def __init__(self, used_keyword):
-        self.create_line = lambda x, f: f"print({f}'{x}')"
         super().__init__(used_keyword)
         self.canonical = 'print'
         self.assignment_possible = False
+        self.maybe_fstring = lambda x, f: f"print({f}'{x}')"
 
     def handle_atoms(self):
-        is_single_atom = len(self.atoms) == 1
-
-        if is_single_atom:
+        multiple_atoms = len(self.atoms) > 1
+        if multiple_atoms:
+            condition = lambda a: a.is_dotted and a.has_builtins()
+        else:
             condition = lambda a: xnor(a.digit_or_builtins_or_self(),
                                        a.is_dotted)
-            dblquote = False
-        else:
-            condition = lambda a: a.is_dotted and a.has_builtins()
-            dblquote = True
 
+        any_dotted_or_builtins = self.process(try_brackets=multiple_atoms,
+                                              condition=condition,
+                                              dblquote=multiple_atoms)
+
+        if not multiple_atoms:
+            return f'{self.canonical}({self.atoms[0].result})'
+
+        r_side = ' '.join(atom.result for atom in self.atoms).strip()
+
+        f = ''
+        if any_dotted_or_builtins:
+            f = 'f'
+
+        return self.maybe_fstring(r_side, f)
+
+    def process(self, try_brackets, condition, dblquote):
+        any_dotted_or_builtins = False
         for atom in self.atoms:
             atom.parenthesize_builtins()
             if condition(atom):
                 atom.stringify_subject(dblquote)
             atom.close_parenthesis(around=atom.subject)
 
-            if not is_single_atom:
+            if try_brackets:
                 if atom.dotted_or_builtins_or_self():
+                    any_dotted_or_builtins = True
                     atom.result = f'{{{atom.result}}}'
+        return any_dotted_or_builtins
 
-        if is_single_atom:
-            return f'{self.canonical}({self.atoms[0].result})'
-
-        return self._convert()
-
-    def _convert(self):
-        any_dotted_or_builtins = any(atom.dotted_or_builtins_or_self() for atom in self.atoms)
+    """def _convert(self, any_dotted_or_builtins):
+        # any_dotted_or_builtins = any(atom.dotted_or_builtins_or_self() for atom in self.atoms)
 
         r_side = ' '.join(atom.result for atom in self.atoms)
 
         converted = self.create_line(r_side.strip(), 'f' if any_dotted_or_builtins else '')
-        return converted
+        return converted"""
--- src/operators/str_operator.py
+++ src/operators/str_operator.py
@@ -1,15 +1,34 @@
+from src.utils import xnor
 from . import PrintOperator
 
 
 class StrOperator(PrintOperator, cls_keywords=('str', "'", '"', "''", '""')):
+
     def __init__(self, used_keyword):
         super().__init__(used_keyword)
         self.assignment_possible = True
-        if '"' in self.used_keyword:
-            self.create_line = lambda x, f: f'{f}"{x}"'
-        else:
-            self.create_line = lambda x, f: f"{f}'{x}'"
+        self.canonical = 'str'
 
-    # don't remove, doesn't handle single atom differently than multiple
+        if '"' in self.used_keyword:
+            self.maybe_fstring = lambda x, f: f'{f}"{x}"'
+        else:
+            self.maybe_fstring = lambda x, f: f"{f}'{x}'"
+
     def handle_atoms(self):
-        return self._handle_multiple_atoms()
+        is_single_atom = len(self.atoms) == 1
+        if is_single_atom:
+            condition = lambda a: a.is_dotted
+        else:
+            condition = lambda a: a.has_self
+
+        any_dotted_or_builtins = self.process(try_brackets=True,
+                                              condition=condition,
+                                              dblquote=False)
+
+        r_side = ' '.join(atom.result for atom in self.atoms).strip()
+
+        f = ''
+        if any_dotted_or_builtins:
+            f = 'f'
+
+        return self.maybe_fstring(r_side, f)
